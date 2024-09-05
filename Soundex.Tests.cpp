ab3947e8cb667076064bb95eaf428c5e797820dd
Nikhilkharvi <54076747+Nikhilkharvi@users.noreply.github.com>
2024-09-05 13:00:30 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,6 +2,15 @@
 #include "Soundex.h"
 #include "Soundex.cpp"
 
+bool isPrevLetterHWY(const std::string& name, size_t& i);
+char getSoundexCode(char c);
+void generateSoundexForPrevLetterHWY(const std::string& name, size_t& i, std::string& soundex, char& lastCode);
+void generateSoundexForCurrentLetter(const std::string& name, size_t& i, std::string& soundex, char& previousCode);
+void handleSoundexLengthFour(std::string& soundex);
+void generateSoundexForLetter(const std::string& name, size_t& i, std::string& soundex, char& previousCode, char& lastCode);
+void handleSoundex(std::string& soundex, const std::string& name);
+std::string generateSoundex(const std::string& name);
+
 TEST_F(SoundexTest, isPrevLetterHWYPass) {
   std::string name = "Tymczak";
   size_t i = 2;
