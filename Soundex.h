43bdde7081dde6023c501255b6c9ac25cb2558db
Nikhilkharvi <54076747+Nikhilkharvi@users.noreply.github.com>
2024-09-05 13:51:15 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,16 +1,18 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
-
+ 
+#include <iostream>
 #include <string>
-#include "Soundex.cpp"
-
+#include <cctype>
+#include <map>
+ 
 std::string generateSoundex(const std::string& name);
 bool isPrevLetterHWY(const std::string& name, size_t& i);
 char getSoundexCode(char c);
-void generateSoundexForPrevLetterHWY(const std::string& name, size_t& i, std::string& soundex, char& lastCode);
-void generateSoundexForCurrentLetter(const std::string& name, size_t& i, std::string& soundex, char& previousCode);
-void handleSoundexLengthFour(std::string& soundex);
+void generateSoundexIfPrevLetterHWY(const std::string& name, size_t& i, std::string& soundex, char& lastCode);
+void generateSoundexIfPrevLetterNotHWY(const std::string& name, size_t& i, std::string& soundex, char& previousCode);
+void makeSoundeLengthFour(std::string& soundex);
 void generateSoundexForLetter(const std::string& name, size_t& i, std::string& soundex, char& previousCode, char& lastCode);
 void handleSoundex(std::string& soundex, const std::string& name);
-
+ 
 #endif // SOUNDEX_H
